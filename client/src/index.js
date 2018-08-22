c23896bb6f0fa2373895873a1c1fc353681c2b6a
tunguyennnnn <conghoa522@gmail.com>
2018-08-22 19:17:45 -0400

    removing window objects

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,6 @@ import ReactDom from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App'
 
-// const history = createBrowserHistory()
-import io from 'socket.io-client'
-import moment from 'moment'
-window.moment = moment
-window.io = io
 ReactDom.render(
   <Router>
     <App />
